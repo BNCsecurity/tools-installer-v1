9f9f4cd86d1ebb742de1eabc15ec5de7fd3ad1c1
BNCsecurity <53198967+BNCsecurity@users.noreply.github.com>
2019-07-23 15:18:57 +0700

    BNCtools.sh

---
+++ BNCtools.sh
@@ -0,0 +1,17 @@
+          $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+          $$         TOOLS BY MR.GANTAI           $$
+          $$         HANYA SEORANG NEWBIE         $$
+          $$      JIKA TOOLS INI BERMASALAH       $$
+          $$      SILAHKAN HUBUNGI SAYA DI WA :   $$
+          $$             089630058754             $$
+          $$                                      $$
+          $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+  
+               #!/bin/bash
+
+echo "masukkan angka pertama"
+read p
+echo "masukkan angka kedua"
+read q
+tambah=$(echo "$p + $q" | bc)
+echo "hasil penambahan = $tambah"
